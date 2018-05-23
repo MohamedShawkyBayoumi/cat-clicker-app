f764c81c5a8366b9ca8193ddb3aa12a9e60c3590
Mohamed Shawky Bayoumi <mohamedshawkybayoumi@gmail.com>
2018-05-24 00:59:12 +0200

    fix display counter

--- app.js
+++ app.js
@@ -52,13 +52,30 @@ for(let cat of allCats){
 	let display = document.querySelector('.display');
 	catCounter.innerHTML = `<h2>Click on photos</h2>`;
 
-	
+	/*
 	cat.addEventListener('click', () => {
 		cat.counter++;
 		catCounter.innerHTML = `<h2>Counter is ${cat.counter}</h2>`;
 		display.innerHTML = cat.innerHTML;
 	
 	});
+	*/
 
+
+	cats.forEach(catDom => {
+		catDom.addEventListener('click', () => {
+			if(catDom.classList[1] === cat.catname){
+				display.innerHTML = `<div class="cat-display ${cat.catname}"><h3>${cat.catname}</h3> ${cat.img}</div>`;
+				catCounter.innerHTML = `<h2>Counter is ${cat.counter}</h2>`;
+
+				const catDisplay = document.querySelector('.cat-display');
+
+				catDisplay.addEventListener('click', () => {
+					cat.counter++;
+					catCounter.innerHTML = `<h2>Counter is ${cat.counter}<h2>`;
+				});
+			};
+		});
+	});
 
 }
